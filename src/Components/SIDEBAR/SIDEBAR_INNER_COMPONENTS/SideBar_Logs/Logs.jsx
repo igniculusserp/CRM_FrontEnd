010e628e2692449b937290461cbbf2f5af1a7b21
shubham.mishra <shubham.mishra@igniculuss.com>
2024-10-03 13:20:36 +0530

    Working on homePage

--- src/Components/SIDEBAR/SIDEBAR_INNER_COMPONENTS/SideBar_Logs/Logs.jsx
+++ src/Components/SIDEBAR/SIDEBAR_INNER_COMPONENTS/SideBar_Logs/Logs.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FaBars, FaPen, FaTrash } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Chat from './LogComponents/Chat';
 import Extension from './LogComponents/Extension';
