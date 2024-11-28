e4dcd51bf1e756090041e2c4ac2ec83d5f7e1883
Tarun <tarun.raikwar@gmail.com>
2024-11-28 16:45:10 +0530

    Fixed the Sidebar and settings

--- src/Components/SIDEBAR/SidebarBase.jsx
+++ src/Components/SIDEBAR/SidebarBase.jsx
@@ -1,21 +1,27 @@
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from './../SIDEBAR/Header';
-import SidebarBar from './../SIDEBAR/Sidebar';
-import './../../ExternalCSS/ExternalCSS_Settings.css';
-import ChatPopup from './SIDEBAR_SETTING/ChatPopup';
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./../SIDEBAR/Header";
+import SidebarBar from "./../SIDEBAR/Sidebar";
+import "./../../ExternalCSS/ExternalCSS_Settings.css";
+import ChatPopup from "./SIDEBAR_SETTING/ChatPopup";
 
 export default function SidebarBase() {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <div className="flex min-h-screen main_Body_Container">
+    <div className="flex min-h-screen">
       {/* SidebarBar is fixed in place */}
-      <div className="h-screen sticky top-0  side_Bar_Container scrollbar-hidden">
+      <div
+        className={`h-screen sticky top-0 scrollbar-hidden ${
+          toggle ? "Side_Bar_Small" : "Side_Bar_Large"
+        }`}
+      >
         <SidebarBar toggle={toggle} />
       </div>
       {/* Main content and header */}
-      <div className="flex flex-col flex-grow Main_Data_Container">
+      <div  className={`flex flex-col ${
+          toggle ? "Main_Screen_Large" : "Main_Screen_Small"
+        }`}>
         <Header toggle={toggle} setToggle={setToggle} />
         {/* Main content scrolls independently */}
         <div className="flex-grow overflow-auto bg-gray-300 ">
