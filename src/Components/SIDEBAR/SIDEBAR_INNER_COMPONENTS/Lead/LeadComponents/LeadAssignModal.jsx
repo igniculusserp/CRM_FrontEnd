b2235fa11bd47a03f372993bc5d90a119ea4dfa3
iamshreyashjain <shreyashjain41@gmail.com>
2025-01-31 11:07:45 +0530

    multipleAssignModalRes+

--- src/Components/SIDEBAR/SIDEBAR_INNER_COMPONENTS/Lead/LeadComponents/LeadAssignModal.jsx
+++ src/Components/SIDEBAR/SIDEBAR_INNER_COMPONENTS/Lead/LeadComponents/LeadAssignModal.jsx
@@ -1,18 +1,18 @@
-import { useState,  } from "react";
-
+//react
+import { useState } from "react";
+//prop
 import PropTypes from "prop-types";
-
+//axios
 import axios from "axios";
-
+//url
 import { tenant_base_url, protocal_url } from "./../../../../../Config/config";
-
+//name
 import { getHostnamePart } from "../../../SIDEBAR_SETTING/ReusableComponents/GlobalHostUrl";
-
+//hooks
 import useManagedBy from "../../../../../Hooks/ManagedBy/useManagedBy";
-
-
-import { FaAngleDown } from "react-icons/fa";
+//icons
 import { ImCancelCircle } from "react-icons/im";
+import { FaAngleDown } from "react-icons/fa";
 
 const LeadAssignModal = ({ onClose }) => {
   const bearer_token = localStorage.getItem("token");
@@ -75,10 +75,8 @@ const LeadAssignModal = ({ onClose }) => {
   };
 
   // ------------------------------------Assigned To Dropdown -----------------------------------
-
-
   const [defaultTextassigned_ToDropDown, setdefaultTextassigned_ToDropDown] =
-    useState("Select Assigned");
+    useState("Select Managed by");
   const [isDropdownassigned_ToDropDown, setisDropdownassigned_ToDropDown] =
     useState(false);
 
@@ -94,68 +92,67 @@ const LeadAssignModal = ({ onClose }) => {
 
   return (
     <>
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-20">
-      <div className="w-10/12 bg-white rounded-lg sm:w-4/12">
-      <div className="flex items-center justify-center px-2 py-2 text-xl font-medium text-white rounded-t-lg bg-cyan-500">
-        <h2 className="mx-auto">Leads Allotment</h2>
-      <ImCancelCircle onClick={onClose} size={22} />
-        </div>
-        <form onSubmit={handleSubmit} className="px-4">
-          <div className="mt-3">
-            <h1 className="px-4 py-2 font-medium text-white rounded-t-lg text-md bg-cyan-500">
-              Details
-            </h1>
-            <div className="px-1 bg-white border-2 border-t-0 rounded-b-xl border-cyan-500">
-              <div className="grid gap-2 p-2">
-                <div className="flex space-x-4">
-                  <div className="flex flex-col w-full">
-                    <label
-                      htmlFor="leadCount"
-                      className="text-sm font-medium text-gray-700"
-                    >
-                      Lead Count:
-                    </label>
-                    <input
-                      type="number"
-                      name="leadCount"
-                      id="leadCount"
-                      value={leadCount}
-                      onChange={handleInputChange}
-                      className="p-2 mt-1 border border-gray-300 rounded-md outline-none"
-                    />
-                  </div>
-                </div>
-                <div className="flex space-x-4">
-                  <div className="flex flex-col w-full">
-                    <div className="relative flex flex-col w-1/1">
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-20">
+        <div className="w-10/12 rounded-lg bg-white sm:w-4/12">
+          <div className="flex items-center justify-center rounded-t-lg bg-cyan-500 px-2 py-2 text-xl font-medium text-white">
+            <h2 className="mx-auto">Leads Allotment</h2>
+            <ImCancelCircle onClick={onClose} size={22} />
+          </div>
+          <form onSubmit={handleSubmit} className="px-4">
+            <div className="mt-3">
+              <h1 className="text-md rounded-t-lg bg-cyan-500 px-4 py-2 font-medium text-white">
+                Details
+              </h1>
+              <div className="rounded-b-xl border-2 border-t-0 border-cyan-500 bg-white px-1">
+                <div className="grid gap-2 p-2">
+                  <div className="flex space-x-4">
+                    <div className="flex w-full flex-col">
                       <label
-                        htmlFor="assignedTo"
-                        className="text-sm font-medium text-gray-700 outline-none"
+                        htmlFor="leadCount"
+                        className="text-sm font-medium text-gray-700"
                       >
-                        Assigned to
+                        Lead Count:
                       </label>
-                      <div
-                        className="relative"
-                        onClick={toggleDropdownassigned_ToDropDown}
-                        onMouseLeave={() =>
-                          setisDropdownassigned_ToDropDown(false)
-                        }
-                      >
-                        <button
-                          className="flex items-center justify-between w-full p-2 mt-1 border border-gray-300 rounded-md"
-                          id="LeadStatusDropDown"
-                          type="button"
+                      <input
+                        type="number"
+                        name="leadCount"
+                        id="leadCount"
+                        value={leadCount}
+                        onChange={handleInputChange}
+                        className="mt-1 rounded-md border border-gray-300 p-2 outline-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex space-x-4">
+                    <div className="flex w-full flex-col">
+                      <div className="w-1/1 relative flex flex-col">
+                        <label
+                          htmlFor="assignedTo"
+                          className="text-sm font-medium text-gray-700 outline-none"
                         >
-                          {assignedTo === ""
-                            ? defaultTextassigned_ToDropDown
-                            : assignedTo}
-                          <FaAngleDown className="ml-2 text-gray-400" />
-                        </button>
-                        {isDropdownassigned_ToDropDown && (
-                          <div className="absolute z-10 w-full bg-white border border-gray-300 rounded-md top-11">
-                            <ul className="text-sm text-gray-700 ">
-                              {managedBy.map(
-                                ({ key, userName, role }) => (
+                          Assigned to
+                        </label>
+                        <div
+                          className="relative"
+                          onClick={toggleDropdownassigned_ToDropDown}
+                          onMouseLeave={() =>
+                            setisDropdownassigned_ToDropDown(false)
+                          }
+                        >
+                          <button
+                            className="mt-1 flex w-full items-center justify-between rounded-md border border-gray-300 p-2"
+                            id="LeadStatusDropDown"
+                            type="button"
+                          >
+                            {assignedTo === ""
+                              ? defaultTextassigned_ToDropDown
+                              : assignedTo}
+                            <FaAngleDown className="ml-2 text-gray-400" />
+                          </button>
+                          {isDropdownassigned_ToDropDown && (
+                            <div className="absolute top-11 z-10 w-full rounded-md border border-gray-300 bg-white">
+                              <ul className="text-sm text-gray-700">
+                                {managedBy.map(({ key, userName, role }) => (
                                   <li
                                     key={key}
                                     onClick={() =>
@@ -163,42 +160,40 @@ const LeadAssignModal = ({ onClose }) => {
                                         userName,
                                       )
                                     }
-                                    className="block px-4 py-2 border-b cursor-pointer hover:bg-cyan-500 hover:text-white"
+                                    className="block cursor-pointer border-b px-4 py-2 hover:bg-cyan-500 hover:text-white"
                                   >
                                     {userName}-({role})
                                   </li>
-                                ),
-                              )}
-                            </ul>
-                          </div>
-                        )}
+                                ))}
+                              </ul>
+                            </div>
+                          )}
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="grid justify-end grid-cols-2 gap-5 mt-4">
-            <button
-              type="submit"
-              className="p-2 mb-3 text-white border-2 rounded-md bg-cyan-500 hover:bg-white hover:text-cyan-500 border-cyan-500"            
-              disabled={loading}
-            >
-              {loading ? "Sending..." : "Submit"}
-            </button>
-            <button
-            type="button"
-            className="p-2 mb-3 font-semibold text-gray-500 border-2 border-gray-300 rounded-md hover:text-white hover:bg-gray-300"            
-            onClick={onClose}
-          >
-            Close
-          </button>
-           
-          </div>
-        </form>
+            <div className="mt-4 grid grid-cols-2 justify-end gap-5">
+              <button
+                type="submit"
+                className="mb-3 rounded-md border-2 border-cyan-500 bg-cyan-500 p-2 text-white hover:bg-white hover:text-cyan-500"
+                disabled={loading}
+              >
+                {loading ? "Sending..." : "Submit"}
+              </button>
+              <button
+                type="button"
+                className="mb-3 rounded-md border-2 border-gray-300 p-2 font-semibold text-gray-500 hover:bg-gray-300 hover:text-white"
+                onClick={onClose}
+              >
+                Close
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   );
 };
