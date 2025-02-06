e70800ff80d105b0c9e9229d65c35b0a2a80b21f
iamshreyashjain <shreyashjain41@gmail.com>
2025-02-06 14:32:00 +0530

    crm_protectedRoute+

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,15 +1,41 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const storedOtp = localStorage.getItem("otp");
+  const token  = localStorage.getItem("token")
+  const navigate = useNavigate();
+  const timerRef = useRef(null);
 
-  if (!storedOtp || storedOtp.length !== 6) {
-    // Redirect to OTP login if OTP is invalid or absent
-    return <Navigate to="/tenantloginOTP" replace />;
-  }
+  // Function to reset the idle timer
+  const resetTimer = () => {
+    if (timerRef.current){ 
+      clearTimeout(timerRef.current);
+    }
+      timerRef.current = setTimeout(() => {
+      localStorage.removeItem("otp");
+      localStorage.removeItem("token");
+      navigate("/tenantlogin", { replace: true });
+    }, 300000); //5 minutes
+  };
 
-  // If OTP is valid, render the child component
+  useEffect(() => {
+    if (!storedOtp || storedOtp.length !== 6) {
+      return;
+    }
+
+    // Event listeners to detect user activity
+    const events = ["mousemove", "keydown", "scroll", "click", "touchstart"];
+    events.forEach((event) => window.addEventListener(event, resetTimer));
+
+    resetTimer(); // Start the idle timer
+
+    return () => {
+      events.forEach((event) => window.removeEventListener(event, resetTimer));
+      if (timerRef.current) clearTimeout(timerRef.current);
+    };
+  }, [storedOtp]);
+
   return children;
 };
 
